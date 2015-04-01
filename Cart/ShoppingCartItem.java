abe0a897eae71ca55b7067a0dad995fb02cc3294
Rabin Awal <morbid_leathal@hotmail.com>
2015-04-01 09:52:10 +0545

    created ShoppingCartItem.java

---
+++ Cart/ShoppingCartItem.java
@@ -0,0 +1,40 @@
+package cart;
+
+import entity.Product;
+
+/**
+ *
+ * @author Admin
+ */
+public class ShoppingCartItem {
+    Product product;
+    int quantity;
+    
+    public ShoppingCartItem(Product product){
+        this.product = product;
+        quantity = 1;
+    }
+    public Product getProduct(){
+        return product;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public void incrementQuantity(){
+        quantity++;
+    }
+    public void decrementQuantity(){
+        quantity--;
+    }
+    public double getTotal(){
+        //double amount = 0;
+        double amount;
+        
+        //amount = (this.getQuantity() * product.getPrice().doubleValue());
+        amount = (this.getQuantity() * product.getPrice());
+        return amount;
+    }
+}
