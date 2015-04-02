64c8325f8db79122cc0207245e5aa88ea66d6a22
Rabin Awal <morbid_leathal@hotmail.com>
2015-04-03 03:28:52 +0545

    Create ProductFacade.java

---
+++ session/ProductFacade.java
@@ -0,0 +1,27 @@
+
+package session;
+
+import entity.Product;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Admin
+ */
+@Stateless
+public class ProductFacade extends AbstractFacade<Product> {
+    @PersistenceContext(unitName = "e-ShopPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProductFacade() {
+        super(Product.class);
+    }
+    
+}
